8b13cfb0d4567cfe3c9c8de10b3372a842b7f7ba
lauuurars <1120098177@u.icesi.edu.co>
2025-09-25 21:08:53 -0500

    finished exercise 2!

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import GreetingCard from './components/GreetingCard' // exportando nuestro componente para renderizarlo :3
+import LikePost from './components/LikePost'
 
 function App() {
 
@@ -11,14 +12,18 @@ function App() {
     { name: "Miguel", age: 28 }
   ]
 
-  // establecemos los usuarios que la función recibe y el index para que pueda acceder a cada elemento y lo reconozca como único (tipo un id), el key se encarga de identificar esta característica
+  // Ejercicio 1: establecemos los usuarios que la función recibe y el index para que pueda acceder a cada elemento y lo reconozca como único (tipo un id), el key se encarga de identificar esta característica
 
   return (
+    <>
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 p-6">
       {users.map((user, index) => (
         <GreetingCard key={index} name={user.name} age={user.age} />
       ))}
     </div>
+
+    <LikePost />
+    </>
   )
 }
 
---
+++ src/components/LikePost.tsx
@@ -0,0 +1,31 @@
+// importamos useState, este hook actualiza el estado del componente
+
+import { useState } from "react";
+
+function LikePost() {
+    const [like, setLike] = useState(0);
+
+    return (
+        <div className="max-w-md mx-auto bg-white rounded-2xl shadow-md space-y-6 p-6 hover:shadow-lg transition">
+            <h2 className="text-2xl font-bold text-violet-600">
+                Aprendiendo React OMG
+            </h2>
+
+            <p className="text-gray-600 text-sm leading-relaxed">
+                Haciendo 20 ejercicios de React lol
+            </p>
+
+            <div className="flex items-center space-x-3">
+                <button
+                    onClick={() => setLike(like + 1)}
+                    className="flex items-center px-4 py-2 bg-pink-500 text-white rounded-full shadow hover:bg-pink-600 transition"
+                >
+                    ❤️ Like
+                </button>
+                <span className="text-gray-700 font-medium">{like} Likes</span>
+            </div>
+        </div>
+    )
+}
+
+export default LikePost
